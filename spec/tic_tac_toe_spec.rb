aee38a1a1f8f59b8ce8255e6793aa6fba8c1304f
Nate <namoricoo@hotmail.com>
2014-01-08 12:17:47 -0800

    add test for adding 'o' to an (x,y) location on the tables

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -14,6 +14,17 @@ describe TicTacToe do
                       [0, 0, 0],
                       [0, 0, 0]
                     ]
-    tic_tac_toe.place_x_in_matrix(x_position,y_position).should == output    
+    tic_tac_toe.place_x_in_matrix(x_position, y_position).should == output
+  end
+  it 'places an o at the (x,y) location' do
+    tic_tac_toe = TicTacToe.new
+    x_position = 0
+    y_position = 0
+    output = Matrix[
+                      ['o', 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]
+                    ]
+    tic_tac_toe.place_x_in_matrix(x_position, y_position).should == output
   end
 end
