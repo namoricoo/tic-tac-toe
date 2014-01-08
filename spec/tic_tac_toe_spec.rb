9394eb2a4ffcd0a3d18684f61e47de778081e0cb
Nate <namoricoo@hotmail.com>
2014-01-08 10:16:12 -0800

    initial commit write test for tic tac toes class declaration

---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,8 @@
+# Documentation
+require 'spec_helper'
+require_relative '../lib/tic_tac_toe.rb'
+describe 'tic_tac_toe' do
+  it 'initialize the tic tac toe class in a Matrix' do
+    tic_tac_toe.new
+  end
+end
