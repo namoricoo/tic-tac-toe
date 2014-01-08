f78ae842547280bc0101bc62890e4bf9221fa8a2
Nate <namoricoo@hotmail.com>
2014-01-08 11:00:58 -0800

    write test for placing an x on the board

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,10 +2,11 @@ require 'matrix'
 # Creating the tic tac toe class
 class TicTacToe
   def initialize
+    # We use 1 for x, 2 is for o
     @matrix = Matrix[
-                    [0, 0, 0],
-                    [0, 0, 0],
-                    [0, 0, 0]
-                   ]
+                      [0, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]
+                    ]
   end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,4 +5,15 @@ describe TicTacToe do
   it 'initialize the tic tac toe class in a Matrix' do
     TicTacToe.new
   end
+  it 'places an x at the (x,y) location' do
+    tic_tac_toe = TicTacToe.new
+    x_position = 0
+    y_position = 0
+    output = Matrix[
+                      [1, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]
+                    ]
+    tic_tac_toe.place_x_in_matrix(x_position,y_position)
+  end
 end
